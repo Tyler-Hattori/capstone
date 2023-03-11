b9a4b0510a1f65bae3a59afdb9e09f4531de823c
Tyler Hattori <77474488+Tyler-Hattori@users.noreply.github.com>
2023-03-11 12:42:43 -0800

    Update ease_costmap.cpp

--- velma/src/pathing/node/costmaps/ease_costmap.cpp
+++ velma/src/pathing/node/costmaps/ease_costmap.cpp
@@ -18,7 +18,11 @@ private:
 public:
   EaseCostmap() {
     n = ros::NodeHandle("~");
-    ease_cm = n.advertise<nav_msgs::OccupancyGrid>("/algorithms/navigation_algoritms/ease_costmap", 1000);
+      
+    std::string costmap_topic;
+    n.getParam("ease_costmap_topic", costmap_topic);
+      
+    ease_cm = n.advertise<nav_msgs::OccupancyGrid>(costmap_topic, 1000);
     odom = n.subscribe("/odom", 1000, &EaseCostmap::odom_callback, this);
     map = n.subscribe("/map", 1000, &EaseCostmap::map_callback, this);
   }
