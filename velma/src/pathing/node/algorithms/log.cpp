1200a2041adeb02685e4f4e5ac1e4f1103773b8f
Tyler Hattori <77474488+Tyler-Hattori@users.noreply.github.com>
2023-03-17 16:25:21 -0700

    Update log.cpp

--- velma/src/pathing/node/algorithms/log.cpp
+++ velma/src/pathing/node/algorithms/log.cpp
@@ -80,7 +80,7 @@ public:
   void publish_waypoints() {
     pathing::waypoints msg;
       
-    for (int i = 0; i < waypoints.size(); i++) {
+    for (int i = 0; i < int(waypoints.size()); i++) {
       geometry_msgs::PoseWithCovarianceStamped waypoint;
       waypoint.pose.pose.position.x = waypoints[i].x;
       waypoint.pose.pose.position.y = waypoints[i].y;
