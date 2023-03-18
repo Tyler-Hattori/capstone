dff232f41d57aec6c243fb974e58b0d124e61816
Tyler Hattori <77474488+Tyler-Hattori@users.noreply.github.com>
2023-03-18 08:38:52 -0700

    Update log.cpp

--- velma/src/pathing/node/algorithms/log.cpp
+++ velma/src/pathing/node/algorithms/log.cpp
@@ -21,7 +21,7 @@ class Log {
 private:
   ros::NodeHandle n;
   ros::Publisher waypoints_pub;
-  ros::Subscriber log_command;
+  ros::Subscriber log_command_sub;
   ros::Subscriber odom_sub;
   
   Point last_logged_point;
@@ -42,7 +42,7 @@ public:
     n.getParam("distance_between_waypoints", distance_between_waypoints);
     
     waypoints_pub = n.advertise<pathing::waypoints>(waypoints_topic, 1000);
-    log_command = n.subscribe("/log_command", 1, &Log::log_command_callback, this);
+    log_command_sub = n.subscribe("/log_command", 1, &Log::log_command_callback, this);
     odom_sub = n.subscribe(odom_topic, 1, &Log::odom_callback, this);
     
     last_logged_point.x = 0.0; last_logged_point.y = 0.0; 
